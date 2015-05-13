9f300a68375d223f7000eae844d22da15823a872
stridentbean <borglin.me@gmail.com>
2015-05-13 16:14:36 -0700

    implemented functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,62 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var top = -1;
+  var bottom = -1;
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+//    debugger;
+    if (size === 0) {
+      top++;
+      bottom++;
+    } else {
+      top++;
+    }
+    storage[top] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+     var result;
+    // if(storage.hasOwnProperty(bottom)){
+    //   result = storage[bottom];
+    //   delete storage[bottom];
+    //   bottom++;
+    // }
+    if(size > 0) {
+      result = storage[bottom];
+      delete storage[bottom];
+      if(size >  1) {
+        bottom++;
+      }
+
+      size--;
+    } else {
+      console.log('empty');
+    }
+
+    return result;
   };
 
   someInstance.size = function(){
+    // if (top === bottom) {
+    //   return 0;
+    // } else {
+    //   return top-bottom;
+    // }
+    return size;
   };
 
   return someInstance;
 };
+
+// var q = Queue();
+// q.enqueue('a');
+// console.log("size after one enQ: " + q.size());
+// q.enqueue('b');
+// console.log("size after two: " + q.size());
+// q.enqueue('b');
+// console.log("size after two: " + q.size());
