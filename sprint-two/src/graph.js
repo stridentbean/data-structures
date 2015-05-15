bd2d5521301a47f65a2351a3b4bd7c00c1ec9cc2
stridentbean <borglin.me@gmail.com>
2015-05-15 12:34:38 -0700

    implemented graph and removeEdge test

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,58 @@
 
 
 var Graph = function(){
+  this.nodes = {};
+  this.edges = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  var result = false;
+  _.each(this.nodes, function(elem) {
+    if (node === elem) {
+      result = true;
+    }
+  });
+  return result;
 };
 
 Graph.prototype.removeNode = function(node){
+  //remove node
+  if (this.nodes.hasOwnProperty(node)) {
+    delete this.nodes[node];
+  }
+  //remove edge
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var result = false;
+  if (this.nodes.hasOwnProperty(fromNode) && this.nodes.hasOwnProperty(toNode)) {
+    result = this.edges.hasOwnProperty('' + fromNode + ',' + toNode);
+  }
+  return result;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if (this.nodes.hasOwnProperty(fromNode) && this.nodes.hasOwnProperty(toNode)) {
+    this.edges['' + fromNode + ',' + toNode] = true;
+    this.edges['' + toNode + ',' + fromNode] = true;
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if (this.nodes.hasOwnProperty(fromNode) && this.nodes.hasOwnProperty(toNode)) {
+    delete this.edges['' + fromNode + ',' + toNode];
+    delete this.edges['' + toNode + ',' + fromNode];
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.nodes, function(node) {
+    cb.call(this, node);
+  });
 };
 
 /*
