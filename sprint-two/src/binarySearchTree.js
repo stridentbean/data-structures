11e45444df97f282776a3b0d83237755a2cd1681
stridentbean <borglin.me@gmail.com>
2015-05-15 19:59:09 -0700

    implemented first two tests of binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,37 @@
 var BinarySearchTree = function(value){
+  var newTree = Object.create(binaryTreeMethods);
+
+  newTree.value = value;
+  newTree.right = undefined;
+  newTree.left = undefined;
+
+  return newTree;
+};
+
+binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  if(value > this.value) {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value) {
+
+};
+
+binaryTreeMethods.depthFirstLog = function() {
+
 };
 
 
